fcf15728c9d240fddd673e6487f916779552bf31
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-09-25 16:31:06 +0200

    Primo Commit

---
+++ _next/static/eNSNR_OIOJguUbgUamb78/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e93b66fb098321e1.css","static/chunks/pages/index-e6c7d426ac44aec3.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
